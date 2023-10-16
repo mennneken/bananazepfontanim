0c33d0e49675a63b5be14c9df28c2d0121f87266
Mennneken <nathanhage@gmail.com>
2023-10-16 18:28:30 +0200

    base project

--- build/main.js
+++ build/main.js
@@ -1,30 +1,23 @@
 const controlVariableWidth = document.getElementById("text-width-input");
-const outputVariableWidthReturn = document.getElementById(
-    "text-width-value-output"
-);
+const outputVariableWidthReturn = document.getElementById("text-width-value-output");
 const controlSize = document.getElementById("text-size-input");
 const tryText = document.getElementsByClassName("test-text");
-
 window.onload = () => {
     console.log(tryText);
     setValueOutput(controlVariableWidth, outputVariableWidthReturn);
     setSizeValueOfText(controlSize.value);
     setWidthValueOfText(controlVariableWidth.value);
 };
-
 controlVariableWidth.addEventListener("input", () => {
     setValueOutput(controlVariableWidth, outputVariableWidthReturn);
     setWidthValueOfText(controlVariableWidth.value);
 });
-
 controlSize.addEventListener("input", () => {
     setSizeValueOfText(controlSize.value);
 });
-
 function setValueOutput(elementSource, elementTarget) {
     elementTarget.innerHTML = elementSource.value;
 }
-
 function setSizeValueOfText(value) {
     Array.from(tryText).forEach((element) => {
         element.style.fontSize = `${value}em`;
